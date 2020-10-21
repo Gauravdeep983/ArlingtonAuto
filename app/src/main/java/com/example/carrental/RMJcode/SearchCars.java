32c943440bc75ca13f95aadc3c3d4f395222fa58
jeeveshsehgal <jeeveshsehgal@gmail.com>
2020-10-20 21:22:45 -0500

    UI for Search for cars and view available cars

---
+++ app/src/main/java/com/example/carrental/RMJcode/SearchCars.java
@@ -0,0 +1,16 @@
+package com.example.carrental.RMJcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.carrental.R;
+
+public class SearchCars extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_cars);
+    }
+}
---
+++ app/src/main/java/com/example/carrental/RMJcode/ViewAvailableCars.java
@@ -0,0 +1,17 @@
+package com.example.carrental.RMJcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.carrental.R;
+
+public class ViewAvailableCars extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_available_cars);
+    }
+}
+
